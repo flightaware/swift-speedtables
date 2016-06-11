e60d0e60b6ff37e1af948099f0ff1727c051a890
Peter da Silva <peter@flightaware.com>
2016-06-11 18:23:10 -0500

    Adding C skiplist skeleton for speedup.

---
+++ skiplists/skiplists.c
@@ -0,0 +1,9 @@
+//
+//  skiplists.c
+//  skiplists
+//
+//  Created by Peter da Silva on 6/11/16.
+//  Copyright © 2016 Flightaware. All rights reserved.
+//
+
+#include "skiplists.h"
---
+++ skiplists/skiplists.h
@@ -0,0 +1,14 @@
+//
+//  skiplists.h
+//  skiplists
+//
+//  Created by Peter da Silva on 6/11/16.
+//  Copyright © 2016 Flightaware. All rights reserved.
+//
+
+#ifndef skiplists_h
+#define skiplists_h
+
+#include <stdio.h>
+
+#endif /* skiplists_h */
